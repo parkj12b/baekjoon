a9732935c23fe9069270ebcb0f93912464149d25
Jacob P <50326088+parkj12b@users.noreply.github.com>
2023-07-30 18:26:02 +0900

    [Gold III] Title: 중량제한, Time: 616 ms, Memory: 58448 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,77 +1,89 @@
 import java.io.*;
 import java.util.*;
 
-class Bridge {
-    int destination;
-    int weight;
-
-    public Bridge(int destination, int weight) {
-        this.destination = destination;
-        this.weight = weight;
-    }
-}
-
 public class Main {
     static int min = 0, max = Integer.MAX_VALUE, start, end;
+    static ArrayList<ArrayList<Node>> bridges;
     static int[] visited;
-    static ArrayList<ArrayList<Bridge>> bridges;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = reader.readLine().split("\\s+");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
+        StringTokenizer st;
+
+        st = new StringTokenizer(reader.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         bridges = new ArrayList<>();
+
         for (int i = 0; i <= N; i++) {
-            bridges.add(new ArrayList<Bridge>());
+            bridges.add(new ArrayList<>());
         }
+        int A = 0;
+        int B = 0;
+        int C = 0;
 
         for (int i = 0; i < M; i++) {
-            input = reader.readLine().split("\\s+");
-            int A = Integer.parseInt(input[0]);
-            int B = Integer.parseInt(input[1]);
-            int C = Integer.parseInt(input[2]);
+            st = new StringTokenizer(reader.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
 
-            bridges.get(A).add(new Bridge(B, C));
-            bridges.get(B).add(new Bridge(A, C));
-            min = Math.min(min, C);
-            max = Math.max(max, C);
-        }
-
-        input = reader.readLine().split("\\s+");
-        start = Integer.parseInt(input[0]);
-        end = Integer.parseInt(input[1]);
-
-        while (min <= max) {
-            int mid = (min + max) / 2;
-            visited = new int[N + 1];
-            if (bfs(mid)) {
-                min = mid + 1;
-            } else {
-                max = mid - 1;
+            bridges.get(A).add(new Node(B,C));
+            bridges.get(B).add(new Node(A,C));
+            if (C < min) {
+                min = C;
+            }
+            if (C > max) {
+                max = C;
             }
         }
 
+        st = new StringTokenizer(reader.readLine());
+
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+
+        while(min <= max){
+            int mid = (min+max)/2;
+            visited = new int[N+1];
+            if(bfs(mid)){
+                min = mid+1;
+            } else {
+                max = mid-1;
+            }
+        }
         System.out.println(max);
     }
 
-    static boolean bfs(int cost) {
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        visited[start] = 1;
+    static boolean bfs(int cost){
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
 
-        while (!q.isEmpty()) {
-            int temp = q.poll();
-            if (temp == end)
+        queue.add(start);
+        visited[start] = 1;
+        while(!queue.isEmpty()){
+            int temp = queue.poll();
+            if(temp == end){
                 return true;
-            for (Bridge bridge : bridges.get(temp)) {
-                if (bridge.weight >= cost && visited[bridge.destination] == 0) {
-                    visited[bridge.destination] = 1;
-                    q.add(bridge.destination);
+            }
+            for(Node node : bridges.get(temp)){
+                if(node.weight >= cost && visited[node.index] == 0){
+                    queue.add(node.index);
+                    visited[node.index] = 1;
                 }
             }
         }
         return false;
     }
+
+    static class Node {
+        int index, weight;
+        Node(int index, int weight){
+            this.index = index;
+            this.weight = weight;
+        }
+    }
+    
+
 }
